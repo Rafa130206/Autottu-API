6e749b382bc95857093d146af6c14032c7b24974
Rafael-Lima <162456786+Rafa130206@users.noreply.github.com>
2025-09-25 19:04:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,12 +21,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
