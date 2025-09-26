3bc6b45c6d21e899d2b11c72c840be0d794f96d9
Rafael-Lima <162456786+Rafa130206@users.noreply.github.com>
2025-09-25 22:15:02 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,12 +21,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
