5858217963d64db7e58db108f9d294a44bb3affe
Rafael-Lima <162456786+Rafa130206@users.noreply.github.com>
2025-09-25 21:06:56 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate(); // cria o banco/tabelas se não existirem
+    db.Database.Migrate();
 }
 
 app.UseSwagger();
